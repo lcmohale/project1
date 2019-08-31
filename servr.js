a115d7b211e72fe3b40d08c62f5b1451ba0f0adc
LCMohale <lcmohale@gmail.com>
2019-08-31 09:37:41 +0200

    Update servr.js

--- servr.js
+++ servr.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname,'views'));
 
 //Configure Middleware
 //app.use(express.favicon(path.join(__dirname,'public/favicon.ico')))
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'/public')));
 
 
 
